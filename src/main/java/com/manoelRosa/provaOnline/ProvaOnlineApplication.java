7540f73d16b2ccb9a9aa9dc258479621c5fd4aad
Manoel Rosa <guiguel@gmail.com>
2020-07-15 00:57:42 -0300

    Remendos

---
+++ src/main/java/com/manoelRosa/provaOnline/ProvaOnlineApplication.java
@@ -0,0 +1,14 @@
+package com.manoelRosa.provaOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaOnlineApplication {
+
+	public static void main(String[] args) {
+		 
+		SpringApplication.run(ProvaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manoelRosa/provaOnline/ProvaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoelRosa.provaOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
